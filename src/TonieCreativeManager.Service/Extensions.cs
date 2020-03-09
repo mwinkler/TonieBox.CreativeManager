670e8f2ac642bbe046e0e6936ff6e18863359277
Matthias Winkler <mwinkler@gmx.ch>
2020-03-09 22:38:39 +0100

    Refactor emoji keyboard

--- src/TonieCreativeManager.Service/Extensions.cs
+++ src/TonieCreativeManager.Service/Extensions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Web;
 
 namespace TonieCreativeManager.Service
@@ -10,5 +12,33 @@ namespace TonieCreativeManager.Service
         public static string EncodeUrl(this string value) => HttpUtility.UrlEncode(value);
         
         public static string DecodeUrl(this string value) => HttpUtility.UrlDecode(value);
+
+        public static IEnumerable<string> GetCharacters(this string text)
+        {
+            if (text == null)
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            var ca = text.ToCharArray();
+            var characters = new List<string>();
+            
+            for (int i = 0; i < ca.Length; i++)
+            {
+                char c = ca[i];
+                if (c > 65535)
+                    continue;
+                
+                if (char.IsHighSurrogate(c))
+                {
+                    i++;
+                    characters.Add(new string(new[] { c, ca[i] }));
+                }
+                else
+                    characters.Add(new string(new[] { c }));
+            }
+
+            return characters;
+        }
     }
 }
